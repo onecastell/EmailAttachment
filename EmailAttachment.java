2d7cdce692c5ae234fc354218a70fed6ec554711
Franklin Castellanos <bloofrank55@gmail.com>
2018-05-30 20:11:42 -0400

    Update EmailAttachment.java

--- EmailAttachment.java
+++ EmailAttachment.java
@@ -15,7 +15,8 @@ public class EmailAttachment{
 	String userName;
 	String password;
 	Properties props;
-   	int recentFound;
+   int recentFound;  //Index of most recent attachment
+   Message[] messages; //All messages from defined folder
 
    //
 	void initServer(String protocol,String host,int port){
@@ -33,6 +34,22 @@ public class EmailAttachment{
       props.put("mail.imap.user", this.userName);
 	}
    
+   void StartSession() throws NoSuchProviderException,MessagingException,IOException{
+         Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
+			   protected PasswordAuthentication getPasswordAuthentication(){
+				  return new PasswordAuthentication(userName, password);
+			   }
+	});
+         
+        Store store = session.getStore("imaps");
+		  store.connect("imap.gmail.com",this.userName,this.password);
+		  //Define Folder
+		  Folder folder = store.getFolder("inbox");
+		  folder.open(Folder.READ_ONLY); 
+		  //LOG((String)inbox.getMessageCount());  //get message count
+		  messages = folder.getMessages();
+      }
+
    //Default Constructor
 	public EmailAttachment(){
 	   //Initilalize Email Server
@@ -40,35 +57,20 @@ public class EmailAttachment{
 	   //Authenticate
 	   Login("f.castellanos@adcommdigitel.com","5levelsof@"); 
 	   
+          
       try{
-		  //Create Session
-		  Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
-			   protected PasswordAuthentication getPasswordAuthentication(){
-				  return new PasswordAuthentication(userName, password);
-			   }
-			  });
-
-		  Store store = session.getStore("imaps");
-		  store.connect("imap.gmail.com",this.userName,this.password);
-
-		  //Define Folder
-		  Folder folder = store.getFolder("inbox");
-		  folder.open(Folder.READ_ONLY);
-
-		  //LOG((String)inbox.getMessageCount());  //get message count
-		  Message[] messages = folder.getMessages();
+        StartSession();
 		  int max = messages.length - 1;
 		  //LOG(messages[messages.length -1].getSubject());
 		  for (int i=max;i>1;i--){
 			if(messages[i].getContentType().contains("multipart") && messages[i].getSubject().contains("Schedule") ){
 			   Multipart multiPart = (Multipart) messages[i].getContent();
 			   int numPart = multiPart.getCount();
-			   //LOG(Integer.toString(numPart));
 			   for (int partCount = 0; partCount < numPart; partCount++) {
-						MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-				  //part.saveFile("C:\\Users\\MN2SALES2\\Downloads" + "thisd");
-				  if((part.getFileName())!=null){
-					 LOG(part.getFileName() + " index: " + i);
+				   MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+				   part.saveFile("tst.xls");
+				   if((part.getFileName())!=null){
+					   LOG(part.getFileName() + " index: " + i);
                 this.recentFound = i;
 					 System.exit(1);
 				  }

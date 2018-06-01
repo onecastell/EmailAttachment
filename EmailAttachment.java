3ef187cc772a7b63453e00a9d39ee6cec6fb7c7b
Franklin Castellanos <bloofrank55@gmail.com>
2018-06-01 15:09:46 -0400

    Code Cleanup
    rev 06/01/18

--- EmailAttachment.java
+++ EmailAttachment.java
@@ -33,14 +33,13 @@ public class EmailAttachment{
 		this.password = password;
       props.put("mail.imap.user", this.userName);
 	}
-   
+   //
    void StartSession() throws NoSuchProviderException,MessagingException,IOException{
          Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
 			   protected PasswordAuthentication getPasswordAuthentication(){
 				  return new PasswordAuthentication(userName, password);
 			   }
-	});
-         
+	     });
         Store store = session.getStore("imaps");
 		  store.connect("imap.gmail.com",this.userName,this.password);
 		  //Define Folder
@@ -55,30 +54,28 @@ public class EmailAttachment{
 	   //Initilalize Email Server
 	   initServer("imap","imap.gmail.com",993);
 	   //Authenticate
-	   Login("f.castellanos@adcommdigitel.com","5levelsof@"); 
-	   
-          
+	   Login("f.castellanos@adcommdigitel.com","5levelsof@");           
       try{
-        StartSession();
-		  int max = messages.length - 1;
-		  //LOG(messages[messages.length -1].getSubject());
-		  for (int i=max;i>1;i--){
-			if(messages[i].getContentType().contains("multipart") && messages[i].getSubject().contains("Schedule") ){
-			   Multipart multiPart = (Multipart) messages[i].getContent();
-			   int numPart = multiPart.getCount();
-			   for (int partCount = 0; partCount < numPart; partCount++) {
-				   MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-				   part.saveFile("tst.xls");
-				   if((part.getFileName())!=null){
-					   LOG(part.getFileName() + " index: " + i);
-                this.recentFound = i;
-					 System.exit(1);
-				  }
-			   }
-			}
-		  }
+         //
+         StartSession();
+		   int max = messages.length - 1;
+		   for (int i=max;i>1;i--){
+			   if(messages[i].getContentType().contains("multipart") && messages[i].getSubject().contains("Schedule") ){
+			      Multipart multiPart = (Multipart) messages[i].getContent();
+			      int numPart = multiPart.getCount();
+			         for (int partCount = 0; partCount < numPart; partCount++) {
+				         MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+				         part.saveFile("tst.xls");
+				         if((part.getFileName())!=null){
+					         LOG(part.getFileName() + " index: " + i);
+                        this.recentFound = i;
+					         System.exit(1);
+				         }
+			         }
+			    }
+		    }
 
-	  }
+	     }
 		  catch(NoSuchProviderException noProvider){LOG("Provider Exception");}
 		  catch(MessagingException mException){mException.printStackTrace();}
 		  catch(IOException io){io.printStackTrace();}
@@ -92,11 +89,12 @@ public class EmailAttachment{
 //                   }
 	public String getHost(){return this.host;}
 	public void setHost(String host){this.host=host;}
+   
 	public String getPort(){return this.port;}
 	public void setPort(String port){this.port=port;}
    public String getUserName(){return this.userName;}
 
-
+   //Debug logger
 	void LOG(String arg){
 	  System.out.println(arg);
 	}

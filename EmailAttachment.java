3043586122254b54b29d6b9b6196ddd6d0e710c6
Franklin Castellanos <bloofrank55@gmail.com>
2018-09-01 02:27:50 -0400

    Code cleanup and algorithm enhancements

--- EmailAttachment.java
+++ EmailAttachment.java
@@ -1,134 +1,155 @@
 /******************************
-*Author:Franklin Castellanos
-*License:None
-*Revision:082918
- * *
+ * *Author:Franklin Castellanos
+ * *License:None
+ * *Revision:090118
 *******************************/
-import java.util.Properties;
+
 import javax.mail.*;
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.File;
 import javax.mail.internet.MimeBodyPart;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class EmailAttachment{
-	String host;
-	String port;
-	String protocol;
-	String userName;
-	String password;
-	Properties props;
-    int recentFound;  //Index of most recent attachment
-    Message[] messages; //All messages from defined folder
-    long start,stop; //Timer variables
+    private String protocol;
+    private String host;
+    private int port;
+
+    private String userName;
+    private String password;
+
+    private Message recentFound;  //Most recently found attachment
+    private Message[] messages = {}; //All messages from defined folder
+    private Properties props = new Properties();
+    private long start = 0, stop = 0; //Timer variables
 
    //Default Constructor
 	public EmailAttachment(){
 	   //Initilalize Email Server
-	   initServer("imap","imap.gmail.com",993);
-	   //Authenticate
-	   Login("bpsales447@gmail.com","5levelsof@"); //password for testing only
-      try{
-         //
-         StartSession();
-         beginTime();
-         //Check recently found 1
-         String recent = getRecent();
+        serverInit("imap", "imap.gmail.com", 993);
+        //Authenticate User
+        serverLogin("bpsales447@gmail.com", "5levelsof@"); //password for testing only
+        try {
+            //Begin New Session
+            StartSession("inbox");
+
+            //Check recently found 1
+         /*String recent = getRecent();
          if(recent!=null){
             LOG("Has Recent at: " + recent);
             //System.exit(2);
          }
          else
-            LOG("No Recent");
-
-		   int max = messages.length - 50;
-		   for (int i=max;i>1;i--){
-			   if(messages[i].getContentType().contains("multipart") && messages[i].getSubject().contains("Metrics") ){
-			      Multipart multiPart = (Multipart) messages[i].getContent();
-			      int numPart = multiPart.getCount();
-			         for (int partCount = 0; partCount < numPart; partCount++) {
-				         MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-				         if((part.getFileName())!=null && part.getFileName().contains(".xls")){
-                        //part.saveFile("attachment.xls");
-                        setRecent(i);
-                        endTime();
-					         //System.exit(1);//exit program
-				         }
-			         }
-			    }
-		    }
-
-	     }
-		  catch(NoSuchProviderException noProvider){LOG("Provider Exception");}
+            LOG("No Recent");*/
+            filterAttachment("Metrics", ".xlsx");
+        } catch (NoSuchProviderException noProvider) {
+            LOG("Provider Exception");
+        }
 		  catch(MessagingException mException){mException.printStackTrace();}
 		  catch(IOException io){io.printStackTrace();}
 	}
-// 						if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-// 							this part is attachment
-// 							String fileName = part.getFileName();
-//                      LOG(messages[i].getSubject());
-//                      LOG(String.format("%d",i));
-// 							part.saveFile(saveDirectory + File.separator + fileName);
-//                   }
-	public String getHost(){return this.host;}
-	public void setHost(String host){this.host=host;}
-	public String getPort(){return this.port;}
-	public void setPort(String port){this.port=port;}
-    public String getUserName(){return this.userName;}
-   
-    //Initialize JavaMail server (protocol, host and port)
-	void initServer(String protocol,String host,int port){
-	    this.props = new Properties();
-        props.put("mail.store.protocol", protocol);
-		props.put("mail.imap.host", host);
-		props.put("mail.imap.port", port);
-		props.put("mail.imap.socketFactory" , port );
-		props.put("mail.imap.socketFactory.class" , "javax.net.ssl.SSLSocketFactory" );	
-	}
 
-	//Acquire server login credentials
-    void Login(String userName, String password){
+	public String getHost(){return this.host;}
+
+    //Filters attachments based on a defined string and a defined extension.
+    //Returns the first message that matches filter
+    private MimeBodyPart filterAttachment(String filter, String extension) throws MessagingException, IOException {
+        beginTime();
+        for (Message msg : messages) {
+            if (msg.getContent() != null && msg.getSubject() != null) {
+                if (msg.getContentType().contains("multipart") && msg.getSubject().contains(filter)) { //If message has attachment
+                    Multipart multiPart = (Multipart) msg.getContent();
+                    //Traverse Attachment Content
+                    for (int partCount = 0; partCount < multiPart.getCount(); partCount++) {
+                        MimeBodyPart attachmentPart = (MimeBodyPart) multiPart.getBodyPart(1);
+                        if ((attachmentPart.getFileName()) != null && attachmentPart.getFileName().contains(extension)) {
+                            attachmentPart.saveFile("recentAttachment.xlsx");
+                            //setRecent(i);
+                            //System.exit(1);//exit program
+                            LOG(String.format("%s (%s) Saved%n", attachmentPart.getFileName(), msg.getReceivedDate()));
+                            endTime();
+                            return attachmentPart;
+                        }
+                    }
+                }
+            }
+        }
+        // if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+        //This part is attachment
+        // String fileName = part.getFileName();
+        // LOG(messages[i].getSubject());
+        // LOG(String.format("%d",i));
+        // 	part.saveFile(saveDirectory + File.separator + fileName);
+        //}
+
+        //If no matches are found, return empty Array
+        LOG("No Match Found. Try another search.");
+        return (new MimeBodyPart());
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+        props.put("mail.imap.host", host);
+    }
+
+    public String getProtocol() {
+        return this.protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+        props.put("mail.store.protocol", protocol);
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+    public String getUserName(){return this.userName;}
+
+    public void setPort(int port) {
+        this.port = port;
+        props.put("mail.imap.port", port);
+        props.put("mail.imap.socketFactory", port);
+        props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    }
+
+    //Initialize JavaMail server (protocol, host and port)
+    void serverInit(String protocol, String host, int port) {
+        setProtocol(protocol);
+        setHost(host);
+        setPort(port);
+    }
+
+    //Acquire server serverLogin credentials
+    void serverLogin(String userName, String password) {
         this.userName = userName;
         this.password = password;
         //Add username to properties
         props.put("mail.imap.user", this.userName);
     }
 
-    //Start new JavaMail server session
-   void StartSession() throws NoSuchProviderException,MessagingException,IOException{
+    //Start new JavaMail server session within a defined email folder e.g. inbox
+    void StartSession(String folder) throws MessagingException {
          Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
 			   protected PasswordAuthentication getPasswordAuthentication(){
 				  return new PasswordAuthentication(userName, password);
 			   }
 	     });
-        Store store = session.getStore("imaps");
-		  store.connect("imap.gmail.com",this.userName,this.password);
+        Store store = session.getStore(this.getProtocol());
+        store.connect("imap.gmail.com", this.getUserName(), this.password);
 		  //Define Folder
-		  Folder folder = store.getFolder("inbox");
-		  folder.open(Folder.READ_ONLY);
-		  messages = folder.getMessages();
-      }
-   //Save index of most recently found attachment
-   public void setRecent(int index){
-      try{
-         this.recentFound = index;
-         //Save to file
-         BufferedWriter writer = new BufferedWriter(new FileWriter("db.dat"));
-         writer.write(String.valueOf(index));//Convert to string for writing
-         writer.close();
-      }
-      catch(IOException e){e.printStackTrace();}
-      finally{LOG("Index was saved at: "+ index);}
-   }
+        Folder workingFolder = store.getFolder(folder);
+        workingFolder.open(Folder.READ_ONLY);
+        messages = workingFolder.getMessages();
+    }
+
    //Get index of most recently found attachment
    public String getRecent() {
       String result = null;
       try{
-         if (this.recentFound == 0){
+          if (this.recentFound != null) {
             //check local DB File
             BufferedReader reader = new BufferedReader(new FileReader("db.dat"));
             result = reader.readLine();
@@ -137,17 +158,15 @@ public class EmailAttachment{
          else return null;
        }
        catch(FileNotFoundException fnfE){
-         //if no file found 
+         //if no file found
          LOG("DB does not exist");
          return null;
        }
-       catch(IOException ioE){}
-       finally{
-         LOG("Index pulled from file "+ result);
-         return result;
-       }
+       catch(IOException ioE){ioE.printStackTrace();}
+       LOG("Index pulled from file "+ result);
+       return result;
    }
-   
+
    //Debug logger
 	void LOG(Object arg){
 	  System.out.println(arg.toString());
@@ -158,15 +177,32 @@ public class EmailAttachment{
       this.stop = System.currentTimeMillis();
       LOG("Time Spent:" + (stop - start)/1000 + "secs");
    }
-   
+
    @Override
    public String toString(){
      return(String.format("%s%n",getUserName()) ); 
    }
-   public static void main(String []args){
-	    EmailAttachment email = new EmailAttachment();
-	    xlsFormatter formatter = new xlsFormatter();
-        formatter.openWorkbook("attachment.xls");
-        formatter.getSheetMatrix();
-	}
+
+    //Save index of most recently found attachment
+    public void setRecent(Message msg) {
+        try {
+            this.recentFound = msg;
+            //Save to file
+            //BufferedWriter writer = new BufferedWriter(new FileWriter("db.dat"));
+            //writer.write(String.valueOf(index));//Convert to string for writing
+            //writer.close();
+        }
+        //catch(IOException e){e.printStackTrace();}
+        finally {
+            LOG("Recent Message Saved");
+        }
+    }
+    public static void main(String[] args) {
+        EmailAttachment email = new EmailAttachment();
+        //System.out.println(email);
+        //xlsFormatter formatter = new xlsFormatter();
+        //formatter.openWorkbook("attachment.xls");
+        //formatter.getSheetMatrix();
+    }
+
 }
--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# EmailAttachment
-Application stub that fetches email content from an imap Server(GMAIL)
-Scheduler Schematic 
-JavaMail API:
--Pull xls from attachment
--Parse to SQL of other DBMS
--Format to Scheduler Module Spec
-JavaIO:
+Author:Franklin Castellanos
+License:None
+Revision:090118
+
+Application that acquires updated metric reports from an IMAP Server.
+Designed to track MTD employee productivity.
+
+TODO:
 -Save serialized object to file
-Android: 
--Module1:
---Pull data from storage or Cloud
---Parse data and output to form
---Make readable and save to file
--Module2:
---"Prettify" content and export to HTML or JPG
+-Better Authentication method
+-Port to Webapp and Android App
+-Display data in a graph
